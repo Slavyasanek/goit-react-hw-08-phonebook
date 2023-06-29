e95e434b01932f64058bf3ebfe39878885b93221
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-29 19:34:01 +0300

    replace for chakra theme provider

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { ThemeProvider } from 'styled-components';
 import { theme } from 'constants';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
+        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+          <ChakraProvider theme={theme}>
             <App />
-          </ThemeProvider>
+          </ChakraProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
