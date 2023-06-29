7f6124bd827f9d1573f435f4edb5612a902472aa
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-29 16:03:36 +0300

    add theme

---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+export const theme = Object.freeze({
+    colors: {
+        mainText: '#27374D',
+        lightDecorative: '#526D82',
+        mainBcg: '#DDE6ED',
+        oddBcg: '#9DB2BF',
+        white: '#FFFFFF'
+    }
+});
