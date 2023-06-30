cd2f80dec3669ae245ac26a780e715688a2ba6f4
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-30 18:18:07 +0300

    add loader

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -6,13 +6,16 @@ import { Suspense } from "react";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { selectIsRefreshing } from "redux/auth/selectors";
+import { selectIsLoading } from "redux/contacts/selectors";
 
 const SharedLayout = () => {
     const isRefreshing = useSelector(selectIsRefreshing);
+    const isLoading = useSelector(selectIsLoading);
     return (<>
         <Header />
         <Box as="main" minW={"480px"}>
             <Container>
+                {isLoading && <Loader/>}
                 {isRefreshing ? <Loader /> : <Suspense >
                     <Outlet />
                 </Suspense>}
