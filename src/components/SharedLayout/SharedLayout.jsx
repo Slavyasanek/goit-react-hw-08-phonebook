e41714b5c7a7a0a14b4c4e3b95d1e9f9f3edb168
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-30 16:00:55 +0300

    add isRefreshing loader

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,17 +1,21 @@
 import { Box } from "@chakra-ui/react";
 import { Container } from "components/Container/Container";
 import { Header } from "components/Header/Header";
+import { Loader } from "components/Loader/Loader";
 import { Suspense } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { selectIsRefreshing } from "redux/auth/selectors";
 
 const SharedLayout = () => {
+    const isRefreshing = useSelector(selectIsRefreshing);
     return (<>
         <Header />
         <Box as="main" minW={"480px"}>
             <Container>
-                <Suspense >
+                {isRefreshing ? <Loader /> : <Suspense >
                     <Outlet />
-                </Suspense>
+                </Suspense>}
             </Container>
         </Box>
     </>)
